dd1d4f6e3c4d5f41a2e70fcff6d993b01fa7eb61
Mustafa KILINÇ <mustafakilinc429@gmail.com>
2023-08-17 22:41:48 +0300

    Initial Commit

---
+++ AppleURLScheme/AppleURLSchemeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleURLSchemeApp.swift
+//  AppleURLScheme
+//
+//  Created by Mustafa Kılınç on 17.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct AppleURLSchemeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
